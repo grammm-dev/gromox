77a1e4c4f1c11c23a372820934b2bc95c2bd5771
import-helper <null@localhost>
2021-02-19 18:41:07 +0100

    Tree prune for cleaner history join

--- EXCHANGE_SYSTEM/php/lib/env.php
+++
@@ -1,5 +0,0 @@
-<?php
-	
-define("APP_PATH", $_SERVER['DOCUMENT_ROOT'] . "/../");
-
-?>
--- install.sh
+++
@@ -1,6 +0,0 @@
-cd core-system/release/ && ./install.systemd && cd ../../
-cd SYSTEM_ADMIN/release/ && ./install.systemd && cd ../../
-cd DOMAIN_ADMIN/release/ && ./install.systemd && cd ../../
-cd mta-system/release/ && ./install.systemd && cd ../../
-cd mra-system/release/ && ./install.systemd && cd ../../
-cd agent-system/release/ && ./install.systemd && cd ../../
